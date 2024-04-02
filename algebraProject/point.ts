78376d3e7cb75eb32d06ca11a8a65893191609dd
JesusMunnoz <jesusmunozpost@gmail.com>
2024-04-02 21:36:29 +0200

    repaso2 ejercicios 1 y 2

--- algebraProject/point.ts
+++ algebraProject/point.ts
@@ -29,4 +29,34 @@ export class Point {
         return `(${this.x},${this.y})`
     }
 
+    public distanceTolOrigin()/*:number*/{  // si pongo number me da error 
+        let h : number;
+        if (this.x == 0 && this.y == 0){
+            return  0;
+        }
+        else if (this.x == 0){
+            return  this.y;
+        }
+        else if (this.y == 0){
+            return this.x;
+        }
+        else if(this.x != 0 && this.y != 0){
+            h = Math.sqrt(Math.pow(this.x, 2)+Math.pow(this.y, 2))
+            return h;
+        }
+    }
+    public calculateDistance(anotherPoint: Point){
+        let d : number;
+        d = Math.sqrt(Math.pow((this.x - anotherPoint.getX()), 2)+Math.pow((this.y - anotherPoint.getY()), 2));
+        return d;
+    }
+
 }
+
+/*let myPoint : Point = new Point (7,3);
+console.log(myPoint.toString());
+console.log(myPoint.distanceTolOrigin());
+
+let anotherPoint : Point = new Point (-4, 8);
+console.log(anotherPoint.toString());
+console.log(myPoint.calculateDistance(anotherPoint));*/
--- algebraProject/pointTest.ts
+++ algebraProject/pointTest.ts
@@ -1,4 +1,18 @@
 import { Point } from "./point";
 
-let myPoint : Point = new Point (3,2);
+let myPoint : Point = new Point (4,5);
 console.log(myPoint.toString());
+console.log(myPoint.distanceTolOrigin());
+
+/*let myPoint1 : Point = new Point (0,3);
+console.log(myPoint1.toString());
+console.log(myPoint1.distanceTolOrigin());
+
+let myPoint2 : Point = new Point (7,0);
+console.log(myPoint2.toString());
+console.log(myPoint2.distanceTolOrigin());*/
+
+
+let anotherPoint : Point = new Point (-4, 8);
+console.log(anotherPoint.toString());
+console.log(myPoint.calculateDistance(anotherPoint));
